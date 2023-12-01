7b09a69434d8fa656b967413185693ace092b83f
murshidameen <murshidmonzpv@gmail.com>
2023-12-01 19:45:59 +0530

    socket io server and client setup completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {io} from 'socket.io-client'
 import './App.css';
 
 function App() {
+    const [message, setMessage] = useState('');
+  const [room, setRoom] = useState('');
+
+  const handleSendMessage = (e) => {
+   
+  };
+
+  const handleJoinRoom = (e) => {
+   
+  };
   return (
     <div className="flex h-screen bg-gray-100">
       <div className="flex flex-col p-6 bg-white rounded shadow-lg w-1/3">
         <h1 className="text-3xl font-extrabold mb-4 text-center">CHAT APP</h1>
+          <form onSubmit={handleSendMessage}>
         <div className="flex space-x-4 mb-4">
-          <input className="flex-grow border p-2 rounded" type="text" placeholder="Type a message" />
-          <button className="bg-blue-500 text-white px-4 py-2 rounded">Send</button>
-        </div>
-        <div className="flex space-x-4 mb-4">
-          <input className="flex-grow border p-2 rounded" type="text" placeholder="Join room" />
-          <button className="bg-green-500 text-white px-4 py-2 rounded">Join</button>
-        </div>
+            <input
+              className="flex-grow border p-2 rounded"
+              type="text"
+              placeholder="Type a message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}/>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded" type="submit">
+              Send
+            </button>
+          </div>
+        </form>
+         <form onSubmit={handleJoinRoom}>
+          <div className="flex space-x-4 mb-4">
+            <input
+              className="flex-grow border p-2 rounded"
+              type="text"
+              placeholder="Join room"
+              value={room}
+              onChange={(e) => setRoom(e.target.value)}
+            />
+            <button className="bg-green-500 text-white px-4 py-2 rounded" type="submit">
+              Join
+            </button>
+          </div>
+        </form>
       </div>
       <div className="flex-grow bg-gray-200 p-4 rounded ml-4">
         <p className='text-right font-bold text-lg'>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
+
       </div>
     </div>
   );
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,24 @@
+import { create } from "domain";
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const PORT = 3000;
-
+const httpServer = createServer();
+const io = new Server(httpServer, {
+    cors: {
+    origin: "http://localhost:5173/"
+    },
+    pingTimeout: 60000,
+    
+})
 app.get('/', async (req, res) => {
     res.json({ status: true, message: "Our node.js app works" })
 });
 
-app.listen(PORT, () => {
+
+
+httpServer.listen(PORT, () => {
    console.log(`Server is listening on http://localhost:${PORT}`); 
 })
