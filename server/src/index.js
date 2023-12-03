6a924b6db8186b53061fbd93e937f7cbfd1b771f
murshidameen <murshidmonzpv@gmail.com>
2023-12-04 00:26:03 +0530

    initail setup server and client,integrated socket io on server

---
+++ server/src/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const PORT = 3000;
+const httpServer = createServer();
+const io = new Server(httpServer, {
+    cors: {
+    origin: "http://localhost:5173  "
+    },
+    pingTimeout: 60000,
+    
+})
+app.get('/', async (req, res) => {
+    res.json({ status: true, message: "Our node.js app works" })
+});
+
+
+
+
+
+
+httpServer.listen(PORT, () => {
+   console.log(`Server is listening on http://localhost:${PORT}`); 
+})
